7a2281177758df32375aaa3fbd198680f20023bf
JBONES <jalilbones10@gmail.com>
2021-03-06 15:16:42 -0500

    Added money and shop system

--- src/main.c
+++ src/main.c
@@ -4,14 +4,16 @@ struct Player
     int amount_clicked;
     double money;
     double multiplier;
+    double cost;
 } player;
 typedef struct
 {
     GtkWidget *counterlabel;
     GtkWidget *clickButton;
     GtkWidget *moneyLabel;
-    GtkWidget *shopGrid;
-    GtkWidget *shopGridArr[6];
+    GtkWidget *purchaseButton;
+    GtkWidget *multiplierLabel;
+    GtkWidget *costLabel;
 } app_widgets;
 void clicked_btn(GtkButton *button, app_widgets *app_widgets)
 {
@@ -22,7 +24,32 @@ void clicked_btn(GtkButton *button, app_widgets *app_widgets)
     sprintf(text, "Money: %.2f", player.money);
     gtk_label_set_text(GTK_LABEL(app_widgets->moneyLabel), text);
 }
+const char *getButtonText(GtkButton *button)
+{
+    return gtk_button_get_label(button);
+}
+void updateLabels(app_widgets *app_widgets)
+{
+}
+void clicked_shop_btn(GtkButton *button, app_widgets *app_widgets)
+{
+    if (player.money >= player.cost)
+    {
+        player.money -= player.cost;
+        char text[20];
+        sprintf(text, "Money: %.2f", player.money);
+        gtk_label_set_text(GTK_LABEL(app_widgets->moneyLabel), text);
+        printf("%f\n", player.cost);
+        player.cost *= (player.cost / 20);
 
+        sprintf(text, "Cost: %.2f", player.cost);
+        gtk_label_set_text(GTK_LABEL(app_widgets->costLabel), text);
+
+        player.multiplier += 10;
+        sprintf(text, "Multiplier: %.2f", player.multiplier);
+        gtk_label_set_text(GTK_LABEL(app_widgets->multiplierLabel), text);
+    }
+}
 int main(int argc, char *argv[])
 {
     GtkBuilder *builder;
@@ -37,9 +64,14 @@ int main(int argc, char *argv[])
     widgets->counterlabel = GTK_WIDGET(gtk_builder_get_object(builder, "counterlabel"));
     widgets->clickButton = GTK_WIDGET(gtk_builder_get_object(builder, "clickbtn"));
     widgets->moneyLabel = GTK_WIDGET(gtk_builder_get_object(builder, "moneylabel"));
+    widgets->purchaseButton = GTK_WIDGET(gtk_builder_get_object(builder, "purchaseButton"));
+    widgets->multiplierLabel = GTK_WIDGET(gtk_builder_get_object(builder, "multiplierlabel"));
+    widgets->costLabel = GTK_WIDGET(gtk_builder_get_object(builder, "costLabel"));
+
     player.amount_clicked = 0;
     player.money = 0.00;
     player.multiplier = 1;
+    player.cost = 100;
     gtk_label_set_text(GTK_LABEL(widgets->counterlabel), "Clicked: 0");
 
     gtk_builder_connect_signals(builder, widgets);
